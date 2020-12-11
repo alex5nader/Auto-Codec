e0c6d690883ccdc6265ec583b2267c5077aad586
Alex Habich <me@alexnader.dev>
2020-12-11 15:23:09 -0600

    Add @Retention and @Target

--- processor/src/main/java/dev/alexnader/auto_codec/CodecHolder.java
+++ processor/src/main/java/dev/alexnader/auto_codec/CodecHolder.java
@@ -1,9 +1,12 @@
 package dev.alexnader.auto_codec;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Target(ElementType.PACKAGE)
+@Retention(RetentionPolicy.SOURCE)
 public @interface CodecHolder {
     String value();
 }
--- processor/src/main/java/dev/alexnader/auto_codec/Record.java
+++ processor/src/main/java/dev/alexnader/auto_codec/Record.java
@@ -1,9 +1,12 @@
 package dev.alexnader.auto_codec;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
 public @interface Record {
     String value() default "";
 }
